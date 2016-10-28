a53e78e6ac19346c534707093b2bac8a62a83c85
Zachariah Moreno <xachmoreno@gmail.com>
2016-10-28 13:29:43 -0700

    Update reqSanitizr.js

--- reqSanitizr.js
+++ reqSanitizr.js
@@ -4,7 +4,7 @@
     var reqSanitizr = function(req) {
         for(var index in req.body) {
             var dirty = req.body[index];
-            var clean = dirty.replace(/[^A-Za-z0-9.,?@]/g, '');
+            var clean = dirty.replace(/[^A-Za-z0-9.,?@ ]/g, '');
             req.body[index] = clean;
         }
     };
